4eb03e6a2191e5b0f2b498e71c924c991a484371
sbacarob <sbacarob@outlook.com>
2014-08-20 06:11:46 -0500

    Update refereeComponent.js

--- sportclub.web/src/main/webapp/src/component/refereeComponent.js
+++ sportclub.web/src/main/webapp/src/component/refereeComponent.js
@@ -44,6 +44,9 @@ define(['component/_CRUDComponent', 'controller/toolbarController', 'model/toolb
             this.addButton({name: "Idioma", icon: "glyphicon-question-sign", }, function() {
                  self.componentController.language();
             });
+            Backbone.on(self.componentId + '-referee-show-exp', function(params) {
+                self.componentController.experience(params);
+            });
         }
     });
     return App.Component.RefereeComponent;
