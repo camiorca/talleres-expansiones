517390cfcd97a64062e64e3d67befc600362f9cc
sbacarob <sbacarob@outlook.com>
2014-08-20 04:33:28 -0500

    Update sportModel.js

--- sportclub.web/src/main/webapp/src/model/sportModel.js
+++ sportclub.web/src/main/webapp/src/model/sportModel.js
@@ -35,6 +35,10 @@ define(['model/_sportModel'], function() {
             if(!attrs.name){
                 validationMessage = "The name can't be empty.";
             }
+            if (parseInt(attrs.minAge)>parseInt(attrs.maxAge))
+            {
+                 validationMessage ="La edad mínima no puede ser mayor a la edad máxima.";
+            }
             if(validationMessage.length>0){
                return validationMessage;
             }
